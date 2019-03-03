f0dda08456e0b232fd62bca2250d04616ec7cd9c
Valentin Kuznetsov <vkuznet@gmail.com>
2019-03-03 09:50:06 -0500

    Make code work on TPU

--- keras_ds.py
+++ keras_ds.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+import traceback
 import numpy as np
 import tensorflow as tf
 import keras
@@ -12,29 +13,35 @@ model = tf.keras.Sequential([
 model.compile(optimizer=tf.train.AdamOptimizer(0.001),
                 loss='categorical_crossentropy', metrics=['accuracy'])
 
-data = np.random.random((1000, 32))
-labels = np.random.random((1000, 10))
-
-# Instantiates a toy dataset instance:
-dataset = tf.data.Dataset.from_tensor_slices((data, labels))
-dataset = dataset.batch(32)
-dataset = dataset.repeat()
-print("dataset", type(dataset), dataset)
-
+data = np.array(np.random.random((1000, 32)), dtype=np.float32)
+labels = np.array(np.random.random((1000, 10)), dtype=np.float32)
 epochs = 2
 steps = 30
 vsteps = 1
+batch_size = 10
+tpu = None
+
+
+# Instantiates a toy dataset instance:
+dataset = tf.data.Dataset.from_tensor_slices((data, labels))
+dataset = dataset.batch(batch_size, drop_remainder=True)
+dataset = dataset.repeat()
+print("dataset", type(dataset), dataset)
 
 try: # TPU detection
     tpu = tf.contrib.cluster_resolver.TPUClusterResolver()
     print('### Training on TPU ###')
+except ValueError:
+    traceback.print_exc()
+    print('### Training on GPU/CPU ###')
+
+if tpu:
     # For TPU, we will need a function that returns the dataset
     ds_input_fn = lambda: dataset
     strategy = tf.contrib.tpu.TPUDistributionStrategy(tpu)
     trained_model = tf.contrib.tpu.keras_to_tpu_model(model, strategy=strategy)
     trained_model.fit(ds_input_fn, epochs=epochs, steps_per_epoch=steps,
             validation_data=ds_input_fn, validation_steps=vsteps)
-except ValueError:
-    print('### Training on GPU/CPU ###')
+else:
     model.fit(dataset, epochs=epochs, steps_per_epoch=steps,
             validation_data=dataset, validation_steps=vsteps)
