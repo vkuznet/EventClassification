2fdc3496ee8f561f31df7a7ff7bcc7fa41e4d052
Valentin Kuznetsov <vkuznet@gmail.com>
2019-03-04 09:47:31 -0500

    Further improvements

--- img2tfrecs.py
+++ img2tfrecs.py
@@ -32,13 +32,19 @@ class OptionParser():
             dest="fdir", default="", help="Input directory of data images")
         self.parser.add_argument("--image-shape", action="store",
             dest="image_shape", default="300,300,3", help="Image shape, default 300,300,3 (color PNG)")
+        self.parser.add_argument("--classes", action="store",
+            dest="classes", default=0, help="Total number of classes")
         self.parser.add_argument("--fout", action="store",
             dest="fout", default="", help="Output file")
+        self.parser.add_argument("--nfiles", action="store",
+            dest="nfiles", default=1000, help="Number of files to pack, default 1000")
         self.parser.add_argument("--verbose", action="store_true",
             dest="verbose", default=False, help="verbose output")
 
 def _int64_feature(value):
-    return tf.train.Feature(int64_list=tf.train.Int64List(value=[value]))
+    if not isinstance(value, list):
+        value = [value]
+    return tf.train.Feature(int64_list=tf.train.Int64List(value=value))
 
 def _bytes_feature(value):
     return tf.train.Feature(bytes_list=tf.train.BytesList(value=[value]))
@@ -60,7 +66,7 @@ def get_files_labels(fdir):
             labels.append(0)
     return files, labels
 
-def convert_to(fdir, fout, img_shape, verbose=False):
+def convert_to(fdir, fout, img_shape, classes, nfiles, verbose=False):
     """Converts a dataset to tfrecords."""
     files, labels = get_files_labels(fdir)
     if not fout.endswith('.tfrecords'):
@@ -70,23 +76,32 @@ def convert_to(fdir, fout, img_shape, verbose=False):
     depth = img_shape[2]
     if verbose:
         print("output: {}".format(fout))
-    with tf.python_io.TFRecordWriter(fout) as writer:
-        for idx, fname in enumerate(files):
-            img = np.array(Image.open(fname))
-            image = img.tostring()
-            if verbose:
-                print("fname: {}".format(fname))
-            label = labels[idx]
-            example = tf.train.Example(
-                features=tf.train.Features(
-                    feature={
-                        'height': _int64_feature(rows),
-                        'width': _int64_feature(cols),
-                        'depth': _int64_feature(depth),
-                        'label': _int64_feature(label),
-                        'image': _bytes_feature(image)
-                    }))
-            writer.write(example.SerializeToString())
+    fout_orig = fout
+    counter = 0
+    fout = fout.replace('.tfrecords', '-{}.tfrecords'.format(counter))
+    writer = tf.python_io.TFRecordWriter(fout)
+    for idx, fname in enumerate(files):
+        img = np.array(Image.open(fname))
+        image = img.tostring()
+        if verbose:
+            print("fname: {}".format(fname))
+        label = labels[idx]
+        example = tf.train.Example(
+            features=tf.train.Features(
+                feature={
+                    'height': _int64_feature(rows),
+                    'width': _int64_feature(cols),
+                    'depth': _int64_feature(depth),
+                    'label': _int64_feature(label),
+                    'image': _bytes_feature(image)
+                }))
+        writer.write(example.SerializeToString())
+        if idx and not idx % nfiles:
+            writer.close()
+            fout = fout_orig.replace('.tfrecords', '-{}.tfrecords'.format(counter))
+            counter += 1
+            writer = tf.python_io.TFRecordWriter(fout)
+    writer.close()
 
 def main():
     "Main function"
@@ -95,8 +110,13 @@ def main():
     fdir = opts.fdir
     fout = opts.fout
     img_shape = tuple([int(s) for s in opts.image_shape.split(',')])
+    classes = int(opts.classes)
+    if not classes:
+        print('You must provide number of classes')
+        sys.exit(1)
+    nfiles = int(opts.nfiles)
     verbose = opts.verbose
-    convert_to(fdir, fout, img_shape, verbose)
+    convert_to(fdir, fout, img_shape, classes, nfiles, verbose)
 
 if __name__ == '__main__':
     main()
--- tfrec2img.py
+++ tfrec2img.py
@@ -30,27 +30,11 @@ def run(fname, fout):
     for string_record in record_iterator:
         example = tf.train.Example()
         example.ParseFromString(string_record)
-        
-        height = int(example.features.feature['height']
-                                     .int64_list
-                                     .value[0])
-        
-        width = int(example.features.feature['width']
-                                    .int64_list
-                                    .value[0])
-        
-        depth = int(example.features.feature['depth']
-                                    .int64_list
-                                    .value[0])
-        
-        label = (example.features.feature['label']
-                                    .int64_list
-                                    .value[0])
-        
-        img_string = (example.features.feature['image']
-                                      .bytes_list
-                                      .value[0])
-        
+        height = int(example.features.feature['height'].int64_list.value[0])
+        width = int(example.features.feature['width'].int64_list.value[0])
+        depth = int(example.features.feature['depth'].int64_list.value[0])
+        label = int(example.features.feature['label'].int64_list.value[0])
+        img_string = (example.features.feature['image'].bytes_list.value[0])
         img_1d = np.fromstring(img_string, dtype=np.uint8)
         rec_img = img_1d.reshape((height, width, depth))
         print('shape: ({},{},{}) label: {}'.format(height, width, depth, label))
