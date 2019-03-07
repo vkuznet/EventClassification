ce6b439338c9b9483a0ae98aa6dcbc1b703468c2
Valentin Kuznetsov <vkuznet@gmail.com>
2019-03-07 12:01:27 -0500

    Add missing datetime module

--- keras_dn.py
+++ keras_dn.py
@@ -28,6 +28,7 @@ import sys
 import math
 import time
 import argparse
+from datetime import datetime
 
 # The GPU id to use, usually either "0" or "1";
 #os.environ["CUDA_DEVICE_ORDER"]="PCI_BUS_ID"
